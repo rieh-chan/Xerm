32ffbc5b8335fe202d49088590ab6e7f3b5753a5
Rishav <rishav080402@gmail.com>
2023-06-16 00:13:27 +0530

    routing is working fine at this point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:AdminComponent,
+    path: "admin"
+  },
+  {
+    component: HomeComponent,
+    path: "home"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
